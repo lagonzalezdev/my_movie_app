d14645dbb642557a73b39afc821cbc48630771ba
#lagonzalezdev <gonzalezortizlau@gmail.com>
2023-02-02 18:02:16 -0500

    Create service for movie_direction

--- service/movie_direction.py
+++ service/movie_direction.py
@@ -1,5 +1,5 @@
 from movie_direction import MovieDirector as MovieDirectionModel
-from schemas.movie_direction import MovieDirector
+from schemas.movie_direction import MovieDirection
 
 class MovieDirectionService():
 
@@ -10,3 +10,22 @@ def get_movie_direction(self):
         result = self.db.query(MovieDirectionModel).first()
         return result
 
+def create_movie_direction(self, movie_direction:MovieDirection):
+        new_movie_direction = MovieDirectionModel(
+            movie_id = movie_direction.id,
+            dir_id = movie_direction.dir_id,
+        )
+        self.db.add(new_movie_direction)
+        self.db.commit()
+        return
+
+def update_movie_direction(self, dir_id:int, data:MovieDirection):
+        movie_direction.movie_id = data.id
+        movie_direction = self.db.query(MovieDirectionModel).filter(MovieDirectionModel.dir_id).first()
+        self.db.commit()
+        return
+
+def delete_movie_direction(self, dir_id:int):
+        self.db.query(MovieDirectionModel).filter(MovieDirectionModel.dir_id).delete()
+        self.db.commit()
+        return
